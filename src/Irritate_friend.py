054e3a7fac03c5a37ca246589aa266da37bd0ca7
Gaurav D. Sharma <er.gauravds@gmail.com>
2022-05-12 19:28:32 +0530

    Update Irritate_friend.py

--- src/Irritate_friend.py
+++ src/Irritate_friend.py
@@ -8,8 +8,6 @@
 
 ### configurations
 number_of_times = 10
-### This Contact Name should be in recent chats , Should match exactly as it appears on contacts
-name = 'Contact_Name'
 message = 'Hi! are you there?'
 
 
@@ -43,25 +41,18 @@ def whatsapp_login():
 	while True:
 		time.sleep(1)
 		try:
-			appLoad = driver.find_element_by_xpath("//div[@class='app two']")
+			appLoad = driver.find_element_by_xpath("//div[@title='Type a message']")
 			if appLoad:
-				gotoChathead(name)
+				break
 		except NoSuchElementException:
 			pass
 		finally:
 			print('Login Checked')
 
 def sendMessage(msg='Hi!'):
-	web_obj = driver.find_element_by_xpath("//div[@contenteditable='true']")
+	web_obj = driver.find_element_by_xpath("//div[@title='Type a message']")
 	web_obj.send_keys(msg)
 	web_obj.send_keys(Keys.RETURN)
-
-def gotoChathead(name):
-	recentList = driver.find_elements_by_xpath("//span[@class='emojitext ellipsify']")
-	for head in recentList:
-		if head.text == name:
-			head.click()
-			break
 
 
 ### Main Method
